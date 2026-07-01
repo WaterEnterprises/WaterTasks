4b5d9185478fd602a9d9f494b41dccc816c44732
John Victor <john.victor.crown@gmail.com>
2026-07-01 05:26:22 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'providers/task_provider.dart';
 import 'screens/main_shell.dart';
